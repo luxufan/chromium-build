09c381472526179588cccc960e9b6a890585cd0a
luxufan <luxufan981014@gmail.com>
2024-06-08 09:19:47 +0100

    wip

--- replace.sh
+++ replace.sh
@@ -2,9 +2,9 @@
 #
 for file in *; do
   if [ -f "$file" ] && [[ $file != *test.cc ]] ; then
-    if grep -q "static_cast" "$file"; then
+    if grep -q "[^_]static_cast" "$file"; then
       sed -i '0,/\#include/!b;//a\#include "base/types/casting.h"' $file
-      sed -i 's/static_cast/base::_static_cast/g' $file
+      sed -i -E 's/([^_])static_cast/\1base::_static_cast/g' $file
       echo "$file"
     fi
   fi

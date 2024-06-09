92b579eacf2268806ea0ca8f9771fc77e9d2473a
luxufan <luxufan981014@gmail.com>
2024-06-09 17:45:02 +0100

    update replace scripts

--- replace.sh
+++ replace.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #
-for file in *; do
-  if [ -f "$file" ] && [[ $file != *test.cc ]] ; then
-    if grep -q "[^_]static_cast" "$file"; then
-      sed -i '0,/\#include/!b;//a\#include "base/types/casting.h"' $file
-      sed -i -E 's/([^_])static_cast/\1base::_static_cast/g' $file
-      echo "$file"
+for d in "$@"; do
+  for file in $d/*; do
+    if [ -f "$file" ] && [[ $file == *.cc ]] && [[ $file != *test.cc ]] ; then
+      if grep -q "[^_]static_cast" "$file"; then
+        sed -i '0,/\#include/!b;//a\#include "base/types/casting.h"' $file
+        sed -i -E 's/([^_])static_cast/\1base::_static_cast/g' $file
+        echo "$file"
+      fi
     fi
-  fi
+  done
 done
